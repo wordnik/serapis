5dc846a77fba8f4ea61b31deacfd1991336091bb
Manuel Ebert <manuel@1450.me>
2015-11-20 11:57:28 -0800

    Lambda Handler on python2.7

---
+++ wordnik/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2
+# coding=utf-8
+"""
+Setup
+"""
+
+__author__ = "Manuel Ebert"
+__copyright__ = "Copyright 2015, summer.ai"
+__date__ = "2015-11-19"
+__email__ = "manuel@summer.ai"
+
+import boto3
+from wordnik.config import config
+import argparse
+
+
+def set_up_buckets():
+    s3 = boto3.resource('s3', aws_access_key_id=config.aws_access_key, aws_secret_access_key=config.aws_access_secret, region_name=config.region)
+    existing_buckets = [bucket.name for bucket in s3.buckets.all()]
+    if config.bucket not in existing_buckets:
+        print("Creating bucket '{}'".format(config.bucket))
+        s3.create_bucket(Bucket=config.bucket)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Set up buckets')
+    parser.add_argument('--config', dest='config', default="default", help='Config file to use')
+    args = parser.parse_args()
+    config.load(args.config)
+    set_up_buckets()
---
+++ wordnik/tasks.py
@@ -0,0 +1,115 @@
+#!/usr/bin/env python2
+# coding=utf-8
+"""
+Task dispatcher
+"""
+
+__author__ = "Manuel Ebert"
+__copyright__ = "Copyright 2015, summer.ai"
+__date__ = "2015-11-20"
+__email__ = "manuel@summer.ai"
+
+import boto3
+from config import config
+import json
+from textblob import TextBlob
+
+s3 = boto3.resource('s3', region_name=config.region)
+
+
+def write_message(task, message):
+    """Writes a message to the S3 bucket."""
+    s3.Object(config.bucket, task).put(Body=json.dumps(message))
+
+
+def search(message):
+    """Takes a message that must contain at least a word, searches for the word
+    and saves a new message with a detect task. The incoming message is expected
+    to look at least like this:
+
+        {
+            'word': ...
+        }
+
+    The message saved will look like this:
+
+        {
+            word: ...
+            urls: [
+                {
+                    url: ...
+                    author: ...
+                    date: ...
+                    doc: ...
+                },
+                ...
+            ]
+        }
+
+    Where doc contains the parsed body text.
+    """
+    message['urls'] = []
+    # Search Diffbot's cache
+    # if there are enough results in cache:
+    #     add results to message
+    # else:
+    #    Search Bing
+    #    Hit Diffbot's exctract API with result urls
+    write_message('detect', message)
+
+
+def detect(message):
+    """Takes a message that must contain a list of URL objects, each having
+    at least a doc property. This will split the doc of each URL into
+    sentences, and  determine whether each sentence is an FRD or not. Finally
+    it will create a 'rate' message that looks like this:
+
+        {
+            word: ...
+            urls: [
+                {
+                    url: ...
+                    author: ...
+                    date: ...
+                    doc: ...
+                    sentences: [
+                        {
+                        s: ...
+                        frd: ...
+                        }
+                    ]
+                },
+                ...
+            ]
+        }
+
+    Where s is the sentence and frd is the probability of this sentence being an FRD.
+    """
+
+    for url in message['urls']:
+        doc = TextBlob(url['doc'])
+        url['sentences'] = []
+        for sentence in doc.sentences:
+            result = {
+                's': sentence,
+                'frd': 0  # Detect if this is an FRD
+            }
+            url['sentences'].append(result)
+    write_message('rate', message)
+
+
+def rate(message):
+    """
+    ...
+    """
+    for url in message['urls']:
+        for sentence in url['sentences']:
+            sentence['rating'] = 0
+    write_message('save', message)
+
+
+def save(message):
+    """
+    ...
+    """
+    pass
