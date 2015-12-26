9a212d3a88d9834960abd5c19d74915149055c00
Clare Corthell <clarecorthell@gmail.com>
2015-12-25 15:22:25 -0900

    Squashed commit of the following:
    commit d94002f848361159d3bebc14c9592db7aaf31a16
    Author: Clare Corthell <clarecorthell@gmail.com>
    Date:   Fri Dec 25 13:48:04 2015 -0900
    
        rm unneeded libs
    
    commit bbbbc0c993f9c9dcb720e42c51a6d293ecc6b59a
    Author: Clare Corthell <clarecorthell@gmail.com>
    Date:   Fri Dec 25 13:46:21 2015 -0900
    
        persist model to s3
    
    commit 49ea7f8951b7a948b22b3b8d56abd5771d0950f4
    Merge: ccff5fc 3447fdc
    Author: Clare Corthell <clarecorthell@gmail.com>
    Date:   Thu Dec 24 23:48:54 2015 -0900
    
        Merge branch 'clare/frd-model-init' of github.com:summerAI/wordnik into Clare/frd-model-init
    
    commit ccff5fc7a2a4a8a36a8030ecee7e9f213b9b026c
    Author: Clare Corthell <clarecorthell@gmail.com>
    Date:   Thu Dec 24 23:48:20 2015 -0900
    
        get git hash
    
    commit 3447fdc5eac3da7787062729d86f8a247cb134ca
    Author: Manuel Ebert <manuel@1450.me>
    Date:   Wed Dec 23 15:29:28 2015 -0800
    
        Install SciPy in CircleCI
    
    commit 69115415e5a02c15967783838fafe4107a2f5931
    Author: Clare <clarecorthell@users.noreply.github.com>
    Date:   Sun Dec 20 23:25:56 2015 -0800
    
        Update feature_development.md
    
    commit fb7eb188b1b5614da7ae54a68711022385e250a2
    Author: Clare <clarecorthell@users.noreply.github.com>
    Date:   Sun Dec 20 23:25:18 2015 -0800
    
        Update feature_development.md
    
    commit b16835fd7c9da9b62406bdf1e89c25cb487c4bf6
    Author: Clare <clarecorthell@users.noreply.github.com>
    Date:   Sun Dec 20 23:24:52 2015 -0800
    
        Update feature_development.md
    
    commit 368ae98c8056945c63c5c9df9edcf90b3e616f92
    Author: Clare Corthell <clarecorthell@gmail.com>
    Date:   Sun Dec 20 23:23:47 2015 -0800
    
        documentation
    
    commit c776d5d9974e43e7d6dde68b629051b7e0db5afe
    Author: Clare Corthell <clarecorthell@gmail.com>
    Date:   Sun Dec 20 23:14:42 2015 -0800
    
        sorted
    
    commit ea84eb462bf0e1c8a2e690d3281370db0a9a81c7
    Author: Clare Corthell <clarecorthell@gmail.com>
    Date:   Sun Dec 20 23:07:45 2015 -0800
    
        part of speech tag feature chi^2 distance
    
    commit c6482434160615fddfb4e99565cead4e77df3526
    Author: Clare Corthell <clarecorthell@gmail.com>
    Date:   Sun Dec 20 21:51:20 2015 -0800
    
        requirements
    
    commit a875ed23ab520f05bc3fafeb7042a58a5b0a2390
    Author: Manuel Ebert <manuel@1450.me>
    Date:   Thu Dec 17 20:17:02 2015 -0800
    
        Save `search_provider` on url_object
    
        Fixes #50

---
+++ serapis/model_info/feature_development.md
@@ -0,0 +1,60 @@
+## Part-of-Speech Features
+
+### Development
+
+The **top 10 most identifying features** for free-range definitions by part-of-speech tag analysis as measured by the chi-squared test (chi2) are:
+
+| part of speech pattern | [chi^2 distance score](https://github.com/summerAI/wordnik/blob/clare/frd-model-init/serapis/model_info/pos_tag_features_chi2dist.csv) | (fictive) example |
+| --- | --- | --- |
+| _term_ vbz dt | 40.644784189606625 | _term_ conveys this |
+| _term_ vbz    | 19.890285126112712 | _term_ is |
+| vbz dt nn | 11.730307326381473 | is an action |
+| vbz dt    | 10.91237066719685 | shows this |
+| _term_ wdt vbz    | 9.9010813488181686 | _term_ which |
+| dt _term_ vbz | 9.6538475741645939 | a _term_ is |
+| vbz dt jj | 9.1372092998409364 | is a descriptive |
+| nn vbn    | 8.3832595804348031 | word meant |
+| vb nns    | 8.3057406590157896 | describing words |
+
+### Reference
+
+Alphabetical list of part-of-speech tags used in the [Penn Treebank Project](https://www.ling.upenn.edu/courses/Fall_2003/ling001/penn_treebank_pos.html):
+
+| Number | Tag | Description |
+| --- | --- | --- |
+| 1. | CC | Coordinating conjunction |
+| 2. | CD | Cardinal number |
+| 3. | DT | Determiner |
+| 4. | EX | Existential there |
+| 5. | FW | Foreign word |
+| 6. | IN | Preposition or subordinating conjunction |
+| 7. | JJ | Adjective |
+| 8. | JJR | Adjective, comparative |
+| 9. | JJS | Adjective, superlative |
+| 10. | LS | List item marker |
+| 11. | MD | Modal |
+| 12. | NN | Noun, singular or mass |
+| 13. | NNS | Noun, plural |
+| 14. | NNP | Proper noun, singular |
+| 15. | NNPS | Proper noun, plural |
+| 16. | PDT | Predeterminer |
+| 17. | POS | Possessive ending |
+| 18. | PRP | Personal pronoun |
+| 19. | PRP$ | Possessive pronoun |
+| 20. | RB | Adverb |
+| 21. | RBR | Adverb, comparative |
+| 22. | RBS | Adverb, superlative |
+| 23. | RP | Particle |
+| 24. | SYM | Symbol |
+| 25. | TO | to |
+| 26. | UH | Interjection |
+| 27. | VB | Verb, base form |
+| 28. | VBD | Verb, past tense |
+| 29. | VBG | Verb, gerund or present participle |
+| 30. | VBN | Verb, past participle |
+| 31. | VBP | Verb, non-3rd person singular present |
+| 32. | VBZ | Verb, 3rd person singular present |
+| 33. | WDT | Wh-determiner |
+| 34. | WP | Wh-pronoun |
+| 35. | WP$ | Possessive wh-pronoun |
+| 36. | WRB | Wh-adverb |
---
+++ serapis/persist_model.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding=utf-8
+from __future__ import unicode_literals
+from __future__ import absolute_import
+
+__author__ = "Clare Corthell"
+__copyright__ = "Copyright 2015, summer.ai"
+__date__ = "2015-11-25"
+__email__ = "clare@summer.ai"
+
+
+import os
+import logging
+import shutil
+import zipfile
+import datetime
+import tempfile
+import json as json
+
+from serapis.util import get_git_hash
+
+from contextlib import closing
+
+from sklearn.externals import joblib
+from sklearn.metrics import precision_recall_fscore_support
+
+import boto3
+from serapis.config import config
+
+log = logging.getLogger('serapis.persist_model')
+
+s3 = boto3.client(
+    's3',
+    region_name=config.region,
+    aws_access_key_id=config.credentials['aws_access_key'],
+    aws_secret_access_key=config.credentials['aws_access_secret']
+)
+
+
+def vectorizer_to_str(obj):
+    obj_dict = obj.__dict__
+    omit_attrs = ['vocabulary_', 'stop_words_']
+    clean_dict = {k: obj_dict[k] for k in obj_dict.keys() if k not in omit_attrs}
+    return str(clean_dict)
+
+
+class PackagedEstimator(object):
+    def __init__(
+        self,
+        vectorizer, estimator, x_train, y_train, x_test, y_test, feature_names=None,
+        *args, **kwargs
+    ):
+        """
+        Package an estimator with a vectorizer for a predictive package.
+
+        >>> PackagedEstimator(vectorizer, estimator, x_train, y_train, x_test, y_test, feature_names)
+        
+        Stores to local directory and S3
+        Model identified by `model_bucket` attr
+
+        NB: Does _not_ employ versioning, assumes single model (identified by s3 bucket)
+
+        """
+        self._vectorizer = vectorizer
+        self._estimator = estimator
+        self._data = {
+            'x_train':      x_train,
+            'x_train_vec':  vectorizer.transform(x_test),
+            'y_train':      y_train,
+            'x_test':       x_test,
+            'x_test_vec':   vectorizer.transform(x_test),
+            'y_test':       y_test,
+        }
+        precision, recall, fscore, support = precision_recall_fscore_support(
+            y_test,
+            estimator.predict(self._data['x_train_vec'])
+        )
+        now = datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+
+        self.metadata = {
+            'vectorizer':    vectorizer_to_str(vectorizer),
+            'model':         str(estimator),
+            'created_at':    now,
+            'feature_names': feature_names,
+            'git_hash':      get_git_hash(),
+            'precision':     [float(p) for p in precision],
+            'recall':        [float(r) for r in recall],
+            'fscore':        [float(f) for f in fscore],
+            'support':       [int(s) for s in support],
+        }
+        log.info('Initialized PackagedEstimator %s' % now)
+
+    @classmethod
+    def from_file(cls, f):
+        """ Given filename, read file and load estimator """
+        zfile = zipfile.ZipFile(f)
+        extract_dir = tempfile.mkdtemp()
+        try:
+            zfile.extractall(extract_dir)
+            data = {}
+            for filename in zfile.namelist():
+                filename_full = os.path.join(extract_dir, filename)
+                if filename == 'metadata.json':
+                    data['metadata'] = json.loads(open(filename_full, 'rt').read())
+                else:
+                    data[filename[:-4]] = joblib.load(filename_full)
+            return cls(
+                vectorizer=data['vectorizer'],
+                estimator=data['estimator'],
+                x_train=data['x_train'],
+                y_train=data['y_train'],
+                x_test=data['x_test'],
+                y_test=data['y_test'],
+                metadata=data['metadata'],
+            )
+        finally:
+            shutil.rmtree(extract_dir)
+
+    def save(self, path='temp_models', model_bucket='wordnik.1m.frd_models', filename=None):
+        """
+        Save the classifier under current path
+
+        - Checks if dir exists
+        - Saves files to disk (per joblib)
+        - Packs files into zip archive
+        - Saves to s3 bucket
+
+        Replaces a single model in S3. Local models have dateime stamps.
+        No versioning employed in S3.
+
+        """
+        if not filename:
+            filename = 'estimator_' + datetime.datetime.now().strftime('%Y-%m-%d-%H-%M-%S')
+        filename += '.zip'
+
+        try:
+            os.makedirs(path)
+        except OSError:  # directory exists, so we can use it
+            pass
+
+        archive_name = os.path.join(path, filename)
+        # joblib requires dump to disk
+        joblib.dump(self._vectorizer, os.path.join(path, 'vectorizer.bin'), compress=9)
+        joblib.dump(self._estimator, os.path.join(path, 'estimator.bin'), compress=9)
+        joblib.dump(self._data['x_train'], os.path.join(path, 'x_train.bin'), compress=9)
+        joblib.dump(self._data['y_train'], os.path.join(path, 'y_train.bin'), compress=9)
+        joblib.dump(self._data['x_test'], os.path.join(path, 'x_test.bin'), compress=9)
+        joblib.dump(self._data['y_test'], os.path.join(path, 'y_test.bin'), compress=9)
+
+        with open(os.path.join(path, 'metadata.json'), 'wt') as f:
+            f.write(json.dumps(self.metadata))
+
+        with closing(zipfile.ZipFile(archive_name, 'w', zipfile.ZIP_DEFLATED)) as zfile:
+            for fn in ['vectorizer.bin', 'estimator.bin', 'x_train.bin', 'y_train.bin',
+                       'x_test.bin', 'y_test.bin', 'metadata.json']:
+                zfile.write(os.path.join(path, fn), fn)
+        # Upload zipped file to S3
+        try:
+            s3.upload_file(archive_name, model_bucket, 'estimator.zip')
+        except Exception, e:
+            message = "Something went wrong pushing the zip to s3: %s %s" % (e, type(e))
+            log.warning(message)
+            raise Exception(message)
+
+    def get_model(self, model_bucket='wordnik.1m.frd_models'):
+        """ Retrieve the model from s3 """
+        filename = 'temp_models/estimator.zip'
+        try:
+            s3.download_file(model_bucket, 'estimator.zip', filename)
+            return self.from_file(filename)
+        except Exception, e:
+            message = "Something went wrong pulling from s3: %s %s" % (e, type(e))
+            log.warning(message)
+            raise Exception(message)
+
