30a0c20c269ef8accab04f9906ffe13f470f3409
Manuel Ebert <manuel@1450.me>
2016-01-24 16:28:01 -0600

    Kill lxml

--- add.py
+++ add.py
@@ -12,7 +12,7 @@ __date__ = "2015-11-09"
 __email__ = "manuel@summer.ai"
 
 import argparse
-from serapis.config import update_config
+from serapis.config import update_config, config
 from serapis import util
 
 tasks_map = {}
@@ -25,10 +25,20 @@ def add(word):
     print("Added task '{}'".format(message['hashslug']))
 
 
+def add_wordlist(filename):
+    import os
+    key = os.path.split(filename)[-1]
+    config.s3.Object(config.bucket, key).put(Body=open(filename, 'rb'))
+    print("Added wordlist '{}'".format(key))
+
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Add a new word to the pipeline')
-    parser.add_argument('word', help='Word to add')
+    parser.add_argument('word', help='Word or wordlist to add')
     parser.add_argument('--config', dest='config', default="default", help='Config file to use')
     args = parser.parse_args()
     update_config(args.config)
-    add(args.word)
+    if args.word.endswith(".wordlist"):
+        add_wordlist(args.word)
+    else:
+        add(args.word)
