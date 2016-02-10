1fd95ba83b35aa60fc8071039cd5e565fceafc13
Manuel Ebert <manuel@1450.me>
2016-02-10 14:51:42 -0800

    Compress task pipeline

--- serapis/tasks.py
+++ serapis/tasks.py
@@ -50,27 +50,6 @@ def search(message):
             'hashslug': ...
         }
 
-    The message saved will look like this:
-
-        {
-            word: ...
-            'hashslug': ...
-            urls: [
-                {
-                    url: ...
-                    variants: ...
-                    author: ...
-                    date: ...
-                    doc: ...
-                    sentences: [
-                        s: ...
-                        s_clean: ...
-                    ]
-                },
-                ...
-            ]
-        }
-
     Where doc contains the parsed body text.
 
     Args:
@@ -87,46 +66,11 @@ def search(message):
 def detect(message):
     """Takes a message that must contain a list of URL objects, each having
     at least a doc property. This will split the doc of each URL into
-    sentences, and  determine whether each sentence is an FRD or not. Finally
-    it will create a 'rate' message that looks like this:
-
-        {
-            word: ...
-            urls: [
-                {
-                    url: ...
-                    author: ...
-                    date: ...
-                    doc: ...
-                    features: { ... }
-                    sentences: [
-                        {
-                        s: ...
-                        frd: ...
-                        }
-                    ]
-                },
-                ...
-            ]
-        }
-
-    Where s is the sentence and frd is the probability of this sentence being an FRD.
+    sentences, and  determine whether each sentence is an FRD or not.
     """
     batch_tag_sentences(message)
-    for url_object in message['urls']:
-        readability_score(url_object)
-        for idx, sentence in enumerate(url_object['sentences']):
-            sentence_clean = url_object['sentences'][idx]['s_clean']
-            url_object['sentences'][idx]['patterns'] = match_wordnik_rules(sentence_clean)
-            url_object['sentences'][idx]['frd'] = 1 if url_object['sentences'][idx]['patterns'] else 0
-    return write_message('rate', message)
 
-
-def rate(message):
-    """
-    Where s is the sentence and frd is the probability of this sentence being an FRD.
-
-    """
+    # Load Models
     model_pipeline = PackagedModel().get_model()
     git_hash = model_pipeline.metadata['git_hash']
     created_at = model_pipeline.metadata['created_at']
@@ -135,17 +79,20 @@ def rate(message):
     model = model_pipeline._model
     class_idx = np.where(model.classes_ == 1)[0][0]  # index of '1' pred in .predict_proba
 
-    for url in message['urls']:
-        for sentence in url['sentences']:
-            x = vec.transform([sentence['s_clean']])
-            
+    for url_object in message['urls']:
+        readability_score(url_object)
+        for sentence in url_object['sentences']:
+            sentence_clean = sentence['s_clean']
+            sentence_vec = vec.transform([sentence['s_clean']])
+
             # metadata
             sentence['model_git_hash'] = git_hash
             sentence['model_creation_date'] = created_at
             
             # predictions from model
-            sentence['frd'] = model.predict(x)[0]
-            sentence['frd_likelihood'] = round(model.predict_proba(x)[0][class_idx], 4)  # P(Classification as FRD)
+            sentence['patterns'] = match_wordnik_rules(sentence_clean)
+            sentence['frd'] = model.predict(sentence_vec)[0]
+            sentence['frd_likelihood'] = round(model.predict_proba(sentence_vec)[0][class_idx], 4)  # P(Classification as FRD)
 
     return write_message('save', message)
 
